c79a3e7dc4578187216618f27fc34757855b211d
betotorradabeto-boop <betotorradabeto@gmail.com>
2026-07-29 15:51:50 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,41 +1,42 @@
 import SaveFile from "./SaveFile.js";
+import GbaSave from "./GbaSave.js";
 
 const input = document.getElementById("saveInput");
 const status = document.getElementById("status");
 
 input.addEventListener("change", async (event) => {
+
     const file = event.target.files[0];
 
-    if (!file) {
+    if (!file)
         return;
-    }
 
     try {
+
         status.textContent = "Carregando save...";
 
         const buffer = await file.arrayBuffer();
 
         const save = new SaveFile(buffer);
 
+        const gba = new GbaSave(save);
+
+        gba.load();
+
         status.textContent =
             `✅ Save carregado! ${save.getSize()} bytes`;
 
         console.log("========== Pokebox ==========");
-        console.log("Tamanho:", save.getSize());
+        console.log("Save:", save);
+        console.log("GbaSave:", gba);
 
-        const reader = save.getReader();
+    } catch (e) {
 
-        reader.seek(0);
+        console.error(e);
 
-        console.log("U8 :", reader.readU8());
-        console.log("U16:", reader.readU16());
+        status.textContent =
+            "❌ Erro ao carregar o save.";
 
-        reader.seek(0);
-
-        console.log("U32:", reader.readU32());
-
-    } catch (error) {
-        console.error(error);
-        status.textContent = "❌ Erro ao carregar o save.";
     }
+
 });

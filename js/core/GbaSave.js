3d6b28c0baa0425e303f4a6242bac86774d73b2e
betotorradabeto-boop <betotorradabeto@gmail.com>
2026-07-29 17:43:28 -0300

    Update GbaSave.js

--- js/core/GbaSave.js
+++ js/core/GbaSave.js
@@ -3,7 +3,6 @@ import Section from "./Section.js";
 export default class GbaSave {
 
     constructor(saveFile) {
-
         this.saveFile = saveFile;
         this.reader = saveFile.getReader();
 
@@ -13,15 +12,14 @@ export default class GbaSave {
 
         this.activeSlot = -1;
         this.loaded = false;
-
+        this.invalidSections = [];
     }
 
     load() {
-
         this.sections = [];
         this.slotA = [];
         this.slotB = [];
-
+        this.invalidSections = [];
         this.activeSlot = -1;
         this.loaded = false;
 
@@ -34,33 +32,39 @@ export default class GbaSave {
         }
 
         for (let i = 0; i < 28; i++) {
-
             const section = new Section(i, i * 0x1000);
 
             if (!section.load(this.reader)) {
-                return false;
+                this.invalidSections.push({ index: i, reason: "load-failed" });
+                continue;
             }
 
             if (!section.isValidSignature()) {
-                return false;
+                this.invalidSections.push({ index: i, reason: "bad-signature" });
             }
 
             this.sections.push(section);
+        }
 
+        if (this.sections.length < 28) {
+            return false;
         }
 
         this.slotA = this.sections.slice(0, 14);
         this.slotB = this.sections.slice(14, 28);
 
+        if (this.slotA.length !== 14 || this.slotB.length !== 14) {
+            return false;
+        }
+
         const saveIndexA = this.slotA[13]?.saveIndex ?? 0;
         const saveIndexB = this.slotB[13]?.saveIndex ?? 0;
 
-        // Em empate, o Slot B é o mais recente.
+        // Em empate, o Slot B vence.
         this.activeSlot = saveIndexA > saveIndexB ? 0 : 1;
 
         this.loaded = true;
         return true;
-
     }
 
     isLoaded() {
@@ -76,7 +80,6 @@ export default class GbaSave {
     }
 
     getSection(id) {
-
         const sections = this.getActiveSlotSections();
 
         for (const section of sections) {
@@ -86,7 +89,6 @@ export default class GbaSave {
         }
 
         return null;
-
     }
 
     getSectionCount() {
@@ -115,6 +117,10 @@ export default class GbaSave {
         }
 
         return [];
+    }
+
+    getInvalidSections() {
+        return this.invalidSections;
     }
 
 }

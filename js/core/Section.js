8c19be435b43ecf8c7c819af4d0b7758e9176ae0
betotorradabeto-boop <betotorradabeto@gmail.com>
2026-07-29 18:09:04 -0300

    Update Section.js

--- js/core/Section.js
+++ js/core/Section.js
@@ -4,24 +4,8 @@ export default class Section {
     static SECTION_SIZE = 0x1000;
     static SIGNATURE = 0x08012025;
 
-    static CHECKSUM_LENGTHS = {
-        0: 3884,
-        1: 3968,
-        2: 3968,
-        3: 3968,
-        4: 3848,
-        5: 3968,
-        6: 3968,
-        7: 3968,
-        8: 3968,
-        9: 3968,
-        10: 3968,
-        11: 3968,
-        12: 3968,
-        13: 2000,
-    };
-
     constructor(index, offset) {
+
         this.index = index;
         this.offset = offset;
 
@@ -31,17 +15,12 @@ export default class Section {
         this.checksum = 0;
         this.signature = 0;
         this.saveIndex = 0;
+
         this.loaded = false;
+
     }
 
     load(reader) {
-        if (!reader || !reader.data) {
-            return false;
-        }
-
-        if (this.offset + Section.SECTION_SIZE > reader.data.length) {
-            return false;
-        }
 
         reader.seek(this.offset);
 
@@ -55,66 +34,115 @@ export default class Section {
         this.saveIndex = reader.readU32();
 
         this.loaded = true;
+
         return true;
+
     }
 
     isLoaded() {
+
         return this.loaded;
+
     }
 
     isValidSignature() {
+
         return this.signature === Section.SIGNATURE;
+
     }
 
-    getExpectedChecksumSize() {
-        return Section.CHECKSUM_LENGTHS[this.id] ?? Section.DATA_SIZE;
-    }
-
-    calculateChecksum() {
-        const size = this.getExpectedChecksumSize();
-
-        let checksum = 0 >>> 0;
-
-        for (let i = 0; i < size; i += 4) {
-            const word =
-                (this.data[i]) |
-                (this.data[i + 1] << 8) |
-                (this.data[i + 2] << 16) |
-                (this.data[i + 3] << 24);
-
-            checksum = (checksum + (word >>> 0)) >>> 0;
-        }
-
-        checksum = (checksum >>> 16) + (checksum & 0xFFFF);
-        checksum = (checksum >>> 16) + (checksum & 0xFFFF);
-
-        return checksum & 0xFFFF;
-    }
-
-    isValidChecksum() {
-        return this.checksum === this.calculateChecksum();
-    }
-
-    isValid() {
-        return this.isValidSignature() && this.isValidChecksum();
-    }
+    //========================
+    // LEITURA
+    //========================
 
     getByte(offset) {
+
         return this.data[offset];
+
     }
 
     getU16(offset) {
-        return this.data[offset] |
-            (this.data[offset + 1] << 8);
+
+        return (
+            this.data[offset] |
+            (this.data[offset + 1] << 8)
+        );
+
     }
 
     getU32(offset) {
+
         return (
             this.data[offset] |
             (this.data[offset + 1] << 8) |
             (this.data[offset + 2] << 16) |
             (this.data[offset + 3] << 24)
         ) >>> 0;
+
     }
 
-            }
+    //========================
+    // ESCRITA
+    //========================
+
+    setByte(offset, value) {
+
+        this.data[offset] = value & 0xFF;
+
+    }
+
+    setU16(offset, value) {
+
+        this.data[offset] = value & 0xFF;
+        this.data[offset + 1] = (value >> 8) & 0xFF;
+
+    }
+
+    setU32(offset, value) {
+
+        this.data[offset] = value & 0xFF;
+        this.data[offset + 1] = (value >> 8) & 0xFF;
+        this.data[offset + 2] = (value >> 16) & 0xFF;
+        this.data[offset + 3] = (value >> 24) & 0xFF;
+
+    }
+
+    //========================
+    // CHECKSUM
+    //========================
+
+    calculateChecksum() {
+
+        let checksum = 0;
+
+        for (let i = 0; i < this.data.length; i += 4) {
+
+            const word =
+                this.data[i] |
+                (this.data[i + 1] << 8) |
+                (this.data[i + 2] << 16) |
+                (this.data[i + 3] << 24);
+
+            checksum += word >>> 0;
+
+        }
+
+        checksum =
+            (checksum & 0xFFFF) +
+            (checksum >>> 16);
+
+        checksum =
+            (checksum & 0xFFFF) +
+            (checksum >>> 16);
+
+        return checksum & 0xFFFF;
+
+    }
+
+    updateChecksum() {
+
+        this.checksum = this.calculateChecksum();
+
+    }
+
+}

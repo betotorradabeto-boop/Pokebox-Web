043644f7f4d21541fad7e49ad097f796773440b6
betotorradabeto-boop <betotorradabeto@gmail.com>
2026-07-29 16:00:07 -0300

    Update GbaSave.js

--- js/GbaSave.js
+++ js/GbaSave.js
@@ -1,39 +1,48 @@
 import Section from "./Section.js";
 
 export default class GbaSave {
-
     constructor(saveFile) {
-
         this.saveFile = saveFile;
         this.reader = saveFile.getReader();
 
         this.sections = [];
+        this.slotA = [];
+        this.slotB = [];
         this.activeSlot = -1;
         this.loaded = false;
-
     }
 
     load() {
-
         this.sections = [];
+        this.slotA = [];
+        this.slotB = [];
+        this.activeSlot = -1;
+        this.loaded = false;
 
-        // FireRed possui 28 sections
-        // 14 do Slot A e 14 do Slot B
-
-        for (let i = 0; i < 28; i++) {
-
-            const offset = i * 0x1000;
-
-            this.sections.push(
-                new Section(i, offset)
-            );
-
+        if (!this.saveFile) {
+            return false;
         }
 
+        if (this.saveFile.getSize() < 0x20000) {
+            return false;
+        }
+
+        for (let i = 0; i < 28; i++) {
+            const section = new Section(i, i * 0x1000);
+            section.load(this.reader);
+            this.sections.push(section);
+        }
+
+        this.slotA = this.sections.slice(0, 14);
+        this.slotB = this.sections.slice(14, 28);
+
+        const saveIndexA = this.slotA[13]?.saveIndex ?? 0;
+        const saveIndexB = this.slotB[13]?.saveIndex ?? 0;
+
+        this.activeSlot = saveIndexA >= saveIndexB ? 0 : 1;
         this.loaded = true;
 
         return true;
-
     }
 
     isLoaded() {
@@ -56,8 +65,19 @@ export default class GbaSave {
         return this.sections.length;
     }
 
+    getSlotA() {
+        return this.slotA;
+    }
+
+    getSlotB() {
+        return this.slotB;
+    }
+
     getActiveSlot() {
         return this.activeSlot;
     }
 
-}
+    getActiveSlotSections() {
+        return this.activeSlot === 0 ? this.slotA : this.slotB;
+    }
+    }

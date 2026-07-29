618b01de3b7788e956a2b4216c52f3daa05b7c9e
betotorradabeto-boop <betotorradabeto@gmail.com>
2026-07-29 18:10:45 -0300

    Update SaveFile.js

--- js/core/SaveFile.js
+++ js/core/SaveFile.js
@@ -1,21 +1,56 @@
 import BinaryReader from "./BinaryReader.js";
 
 export default class SaveFile {
-constructor(buffer) {
-this.buffer = buffer;
-this.reader = new BinaryReader(buffer);
-}
 
-getReader() {  
-    return this.reader;  
-}  
+    constructor(buffer) {
 
-getBuffer() {  
-    return this.buffer;  
-}  
+        this.buffer = buffer;
+        this.reader = new BinaryReader(buffer);
 
-getSize() {  
-    return this.buffer.byteLength;  
-}
+    }
+
+    getReader() {
+
+        return this.reader;
+
+    }
+
+    getBuffer() {
+
+        return this.buffer;
+
+    }
+
+    getSize() {
+
+        return this.buffer.byteLength;
+
+    }
+
+    download(filename = "Pokebox_Save.sav") {
+
+        const blob = new Blob(
+            [this.buffer],
+            {
+                type: "application/octet-stream"
+            }
+        );
+
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement("a");
+
+        a.href = url;
+        a.download = filename;
+
+        document.body.appendChild(a);
+
+        a.click();
+
+        document.body.removeChild(a);
+
+        URL.revokeObjectURL(url);
+
+    }
 
 }

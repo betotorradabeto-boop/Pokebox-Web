87008177bc1d9977dd89bbb7af0bf644c9582ca6
betotorradabeto-boop <betotorradabeto@gmail.com>
2026-07-29 16:46:51 -0300

    Update Section.js

--- js/Section.js
+++ js/Section.js
@@ -1,31 +1,35 @@
 export default class Section {
-    constructor(index, offset) {
+
+    constructor(index, offset, id, saveIndex, checksum, signature, data) {
+
         this.index = index;
         this.offset = offset;
+        this.id = id;
+        this.saveIndex = saveIndex;
+        this.checksum = checksum;
+        this.signature = signature;
+        this.data = data;
 
-        this.data = new Uint8Array(0x0F80); // 3968 bytes
-        this.id = 0;
-        this.checksum = 0;
-        this.signature = 0;
-        this.saveIndex = 0;
     }
 
-    load(reader) {
-        reader.seek(this.offset);
+    getByte(offset) {
 
-        for (let i = 0; i < 0x0F80; i++) {
-            this.data[i] = reader.readU8();
-        }
+        return this.data[offset];
 
-        this.id = reader.readU16();
-        this.checksum = reader.readU16();
-        this.signature = reader.readU32();
-        this.saveIndex = reader.readU32();
-
-        return true;
     }
 
-    isValidSignature() {
-        return this.signature === 0x08012025;
+    getU16(offset) {
+
+        return this.data[offset] |
+            (this.data[offset + 1] << 8);
+
     }
+
+    getU32(offset) {
+
+        return this.getU16(offset) |
+            (this.getU16(offset + 2) << 16);
+
+    }
+
 }

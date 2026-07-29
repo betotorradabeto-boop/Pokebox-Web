9dd0ec73b29c6ab70887b7aa5a80438974d4a8bc
betotorradabeto-boop <betotorradabeto@gmail.com>
2026-07-29 16:44:20 -0300

    Update TrainerInfo.js

--- js/TrainerInfo.js
+++ js/TrainerInfo.js
@@ -1,1 +1,45 @@
+export default class TrainerInfo {
 
+    constructor() {
+
+        this.name = "";
+        this.trainerId = 0;
+        this.secretId = 0;
+        this.money = 0;
+
+    }
+
+    load(slot) {
+
+        // Ainda vamos implementar a leitura do save.
+        // Por enquanto apenas retorna true.
+
+        return true;
+
+    }
+
+    getName() {
+
+        return this.name;
+
+    }
+
+    getTrainerId() {
+
+        return this.trainerId;
+
+    }
+
+    getSecretId() {
+
+        return this.secretId;
+
+    }
+
+    getMoney() {
+
+        return this.money;
+
+    }
+
+}

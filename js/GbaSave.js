d895f7753fcf86c04b38515ae4fb3ec48ccad7f4
betotorradabeto-boop <betotorradabeto@gmail.com>
2026-07-29 16:48:09 -0300

    Update GbaSave.js

--- js/GbaSave.js
+++ js/GbaSave.js
@@ -1,21 +1,27 @@
 import Section from "./Section.js";
 
 export default class GbaSave {
+
     constructor(saveFile) {
+
         this.saveFile = saveFile;
         this.reader = saveFile.getReader();
 
         this.sections = [];
         this.slotA = [];
         this.slotB = [];
+
         this.activeSlot = -1;
         this.loaded = false;
+
     }
 
     load() {
+
         this.sections = [];
         this.slotA = [];
         this.slotB = [];
+
         this.activeSlot = -1;
         this.loaded = false;
 
@@ -28,9 +34,13 @@ export default class GbaSave {
         }
 
         for (let i = 0; i < 28; i++) {
+
             const section = new Section(i, i * 0x1000);
+
             section.load(this.reader);
+
             this.sections.push(section);
+
         }
 
         this.slotA = this.sections.slice(0, 14);
@@ -40,44 +50,77 @@ export default class GbaSave {
         const saveIndexB = this.slotB[13]?.saveIndex ?? 0;
 
         this.activeSlot = saveIndexA >= saveIndexB ? 0 : 1;
+
         this.loaded = true;
 
         return true;
+
     }
 
     isLoaded() {
+
         return this.loaded;
+
     }
 
     getReader() {
+
         return this.reader;
+
     }
 
     getSections() {
+
         return this.sections;
+
     }
 
-    getSection(index) {
-        return this.sections[index];
+    getSection(id) {
+
+        const sections = this.getActiveSlotSections();
+
+        for (const section of sections) {
+
+            if (section.id === id) {
+                return section;
+            }
+
+        }
+
+        return null;
+
     }
 
     getSectionCount() {
+
         return this.sections.length;
+
     }
 
     getSlotA() {
+
         return this.slotA;
+
     }
 
     getSlotB() {
+
         return this.slotB;
+
     }
 
     getActiveSlot() {
+
         return this.activeSlot;
+
     }
 
     getActiveSlotSections() {
-        return this.activeSlot === 0 ? this.slotA : this.slotB;
+
+        return this.activeSlot === 0
+            ? this.slotA
+            : this.slotB;
+
     }
-    }
+
+}
